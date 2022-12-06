8082aa00a33c3eacab08b6fa2668d9ee0fbbf36d
11160101 <11160101@me.mcu.edu.tw>
2022-12-06 11:55:39 +0800

    week14

---
+++ week14/week14-1.cpp
@@ -0,0 +1,20 @@
+///狡策:俱计跑计 int a; a琌俱计
+///狡策: ㄧΑ(量ウ)
+/// int sum( int a, int b ); ウ
+
+#include <stdio.h>
+int sum( int a, int b );
+
+int main()
+{   ///ノぇ玡,璶declare ┪ ﹚竡 define
+    int ans = sum(2,3); ///ㄏノ ㊣
+    printf("ans: %d\n", ans);
+}
+int sum( int a, int b ) ///﹚竡define
+{
+    return a + b;
+}   ///肚 確產
+
+
+
+
