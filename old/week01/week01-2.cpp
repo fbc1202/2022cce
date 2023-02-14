c79a247a004a8f0f4d9f884346fef5b4f9d18074
fbc1202 <11160101@me.mcu.edu.tw>
2023-02-14 11:57:15 +0800

    整理舊程式、加新的week01

---
+++ old/week01/week01-2.cpp
@@ -0,0 +1,18 @@
+#include <stdio.h>
+int main()
+{
+    printf("Hello World\n");
+    printf("Hello World\n");
+    printf("我是馮稟宸\n");
+    ///有時候中文有問題,可以用英文
+    //註解 comments
+    ///CodeBlockb如果畫3條,會比較深
+}
+
+
+
+
+
+
+
+
--- week01/week01-2.cpp
+++ week01/week01-2.cpp
@@ -1,18 +1,14 @@
 #include <stdio.h>
 int main()
 {
-    printf("Hello World\n");
-    printf("Hello World\n");
-    printf("我是馮稟宸\n");
-    ///有時候中文有問題,可以用英文
-    //註解 comments
-    ///CodeBlockb如果畫3條,會比較深
+    printf("請輸入1個字母: ");
+    char c;
+    scanf("%c", &c);
+    printf("你輸入了 %c \n", c);
+
+    printf("請輸入1個整數: ");
+    int a;
+    scanf("%d", &a);
+    printf("你輸入了 %d \n", a);
+    return 0;
 }
-
-
-
-
-
-
-
-
